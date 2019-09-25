1953c1a040276fbbde68bf118ade835faa3f525a
Noi-Git <noi.patsin@gmail.com>
2019-09-24 20:22:42 -0700

    setup environment development key

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
+const API_KEY = `${process.env.REACT_APP_API_KEY}`
+console.log(API_KEY)
+
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID 0ae8edbd67abb83a49e8b53888c83181e4a7d92a2c9055abf48a7889ff0a52b2"
+      `Client-ID ${process.env.REACT_APP_API_KEY}`
   }
 });
